b1c05c6313c035fd3f9e0a69b51f757e61d3dda9
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-08-11 18:57:20 +0100

    Update stringPlus.js

--- stringPlus.js
+++ stringPlus.js
@@ -8,30 +8,31 @@
  class stringPlus {
   
   
-    constructor() {
-    }
-    
+  constructor() {
+  }
+  
 
-    retira_acentos(str){
+  retira_acentos(str){
 
-      var com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
-      var sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
-      var novastr="";
-      for(int i=0; i<str.length; i++) {
-        troca=false;
-        for (a=0; a<com_acento.length; a++) {
-          if (str.substring(i,1)==com_acento.substring(a,1)) {
-            novastr+=sem_acento.substring(a,1);
-            troca=true;
-            break;
-          }
-        }
-        if (troca==false) {
-          novastr+=str.substr(i,1);
+    var com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+    var sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
+    var novastr="";
+    let troca;
+    for(let i=0; i<str.length; i++) {
+      troca=false;
+      for (let a=0; a<com_acento.length; a++) {
+        if (str.substring(i,1)==com_acento.substring(a,1)) {
+          novastr+=sem_acento.substring(a,1);
+          troca=true;
+          break;
         }
       }
-      return novastr;
+      if (troca==false) {
+        novastr+=str.substr(i,1);
+      }
     }
-    
-    
+    return novastr;
   }
+  
+  
+}
