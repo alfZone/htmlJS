d3f23af618c69c9a612688efd557956d12f8733d
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-08-11 18:03:36 +0100

    Update stringPlus.js

--- stringPlus.js
+++ stringPlus.js
@@ -1,1 +1,37 @@
-bb
+/**
+ * @author alf
+ * @copyright 2022
+ * @ver 1.1
+ */
+
+
+ class stringPlus {
+  
+  
+    constructor() {
+    }
+    
+
+    retira_acentos(str){
+
+      var com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+      var sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
+      var novastr="";
+      for(i=0; i<str.length; i++) {
+        troca=false;
+        for (a=0; a<com_acento.length; a++) {
+          if (str.substring(i,1)==com_acento.substring(a,1)) {
+            novastr+=sem_acento.substring(a,1);
+            troca=true;
+            break;
+          }
+        }
+        if (troca==false) {
+          novastr+=str.substr(i,1);
+        }
+      }
+      return novastr;
+    }
+    
+    
+  }
