36d3f0bc4b4b9473718da10d1124b83f1623b62e
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-08-12 12:08:59 +0100

    Update stringPlus.js

--- stringPlus.js
+++ stringPlus.js
@@ -1,38 +1,18 @@
 /**
  * @author alf
  * @copyright 2022
- * @ver 1.2
+ * @ver 2.0
  */
 
 
  class stringPlus {
   
-  
   constructor() {
   }
   
-
   retira_acentos(str){
-
-    var com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
-    var sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
-    var novastr="";
-    let troca;
-    for(let i=0; i<str.length; i++) {
-      troca=false;
-      for (let a=0; a<com_acento.length; a++) {
-        if (str.substring(i,1)==com_acento.substring(a,1)) {
-          novastr+=sem_acento.substring(a,1);
-          troca=true;
-          break;
-        }
-      }
-      if (troca==false) {
-        novastr+=str.substr(i,1);
-      }
-    }
-    return novastr;
+    var semAcento = str.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    return semAcento;
   }
-  
   
 }
