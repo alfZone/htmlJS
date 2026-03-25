c5403b4f25ff627b25ca9081cbbe0da876293da5
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-03-25 11:27:26 +0000

    Update version and refactor URL handling functions

--- html.js
+++ html.js
@@ -1,20 +1,29 @@
 /**
  * @author alf
  * @copyright 2022
- * @ver 4.1
+ * @ver 4.2
  * https://github.com/alfZone/htmlJS
  */
 
-var c = c || new Config();
+//var c = c || new Config();
 
-
-function UrlExists(url){
-    var http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status!=404;
+function UrlExists(url) {
+  var http = new XMLHttpRequest();
+  http.open("HEAD", url, false);
+  http.send();
+  return http.status != 404;
 }
 
+// Função para atualizar a URL sem recarregar a página
+function updateURL(value, parameter = "") {
+  if (parameter == 0) {
+    const newUrl = `${window.location.pathname}/${value}`;
+    window.history.pushState({ value: value }, "", newUrl);
+  } else {
+    const newUrl = `${window.location.pathname}?${parameter}=${value}`;
+    window.history.pushState({ value: value, parameter: parameter }, "", newUrl);
+  }
+}
 
 function includeHTML(tag) {
   var z, i, elmnt, file, xhttp;
@@ -27,15 +36,19 @@ function includeHTML(tag) {
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute(tag);
           includeHTML();
         }
-      }      
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /*exit the function:*/
@@ -46,28 +59,24 @@ function includeHTML(tag) {
 
 //reload a .js file
 function load_js(scriptPath) {
-      var head= document.getElementsByTagName('head')[0];
-      var script= document.createElement('script');
-      script.src= scriptPath;
-      head.appendChild(script);
-   }
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.src = scriptPath;
+  head.appendChild(script);
+}
 
 //get a url parameter indicate by param
-function getURLParam(param){
-   const queryString = window.location.search;
-    //console.log(queryString);
-   const urlParams = new URLSearchParams(queryString);
-   const id = urlParams.get(param)
-   return id
-  
+function getURLParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
 }
 
 //get a url parte indicate by position
-function getURLPos(pos){
+function getURLPos(pos) {
   var queryString = window.location.pathname;
-  queryString=queryString.split("/")
+  queryString = queryString.split("/");
   //queryString=queryString[1].split("/")
-  console.log(queryString[pos]);  
+  console.log(queryString[pos]);
   return queryString[pos];
 }
 
